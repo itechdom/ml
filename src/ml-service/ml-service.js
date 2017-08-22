e7cdaad95164445c00403761d44c9948d1bd75be
Sam Alghanmi <oalghnmi@gmail.com>
2017-08-22 10:15:56 -0400

    added ml service

---
+++ src/ml-service/ml-service.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+// ---------------------------------------------------------
+// get an instance of the router for api routes
+// ---------------------------------------------------------
+var apiRoutes = express.Router();
+
+export default function auth({
+	app
+}) {
+
+  apiRoutes.post('/train',(req,res)=>{
+
+  })
+
+  apiRoutes.post('/test',(req,res)=>{
+
+  })
+
+  apiRoutes.get('/ping',(req,res)=>{
+    res.send("PING!!!");
+  })
+
+	return apiRoutes;
+}
